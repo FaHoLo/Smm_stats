3a94b6313009fd866504fc968ecb4dde388298ef
FaHoLo <47195343+FaHoLo@users.noreply.github.com>
2019-09-23 22:00:15 +0300

    Simplify code with using Counter function

--- Smm_analyze/insta_stats.py
+++ Smm_analyze/insta_stats.py
@@ -2,6 +2,7 @@ import os
 from instabot import Bot
 from dotenv import load_dotenv
 from datetime import datetime, timedelta
+from collections import Counter
 
 
 def main():
@@ -35,55 +36,40 @@ def log_into_instagram(bot):
     bot.login(username=insta_login, password=insta_password)
 
 def get_all_comments_insta(bot, all_posts):
-    all_comments = []
+    all_comments = {}
     for post in all_posts:
-        all_comments.append(bot.get_media_comments_all(post))
+        all_comments.update({post: bot.get_media_comments_all(post)})
     return all_comments
 
 def collect_latest_comments_insta(all_comments, days_number=90):
     now_time = datetime.utcnow()
-    latest_comments = []
-    for post_comments in all_comments:
+    latest_comments = {}
+    for post, comments in all_comments.items():
         post_latest_comments = []
-        for comment in post_comments:
+        for comment in comments:
             comment_time = datetime.fromtimestamp(comment['created_at_utc'])
             if now_time - comment_time > timedelta(days=days_number): continue
-            post_latest_comments.append(comment)
-        latest_comments.append(post_latest_comments)
+            post_latest_comments.append(comment['user_id'])
+        latest_comments.update({post: post_latest_comments})
     return latest_comments
 
 def collect_comments_top(comments, company_id):
-    comments_top = {}
-    for post_comments in comments:
-        for comment in post_comments:
-            commenter_id = comment['user_id']
-            if commenter_id == company_id: continue
-            if commenter_id not in comments_top:
-                comments_top.update({commenter_id: 0})
-            comments_top[commenter_id] += 1
-    return comments_top
+    comments_top = Counter()
+    for commenters in comments.values():
+        for commenter in commenters:
+            comments_top[commenter] += 1
+    del comments_top[company_id]
+    return dict(comments_top.most_common())
 
 def collect_posts_top(comments, company_id):
-    posts_top = {}
-    for post_comments in comments:
-        post_commenters = collect_post_commenters(post_comments, company_id)
-        posts_top = update_posts_top(post_commenters, posts_top)
-    return posts_top
-
-def collect_post_commenters(post_comments, company_id):
-    post_commenters = set()
-    for comment in post_comments:
-        commenter_id = comment['user_id']
-        if commenter_id == company_id: continue
-        post_commenters.add(commenter_id)
-    return post_commenters
-
-def update_posts_top(post_commenters, posts_top):
-    for commenter in post_commenters:
-        if commenter not in posts_top:
-            posts_top.update({commenter: 0})
-        posts_top[commenter] += 1
-    return posts_top
+    posts_top = Counter()
+    for commenters in comments.values():
+        post_commenters = Counter()
+        for commenter in commenters:
+            post_commenters[commenter] = 1
+        posts_top += post_commenters
+    del posts_top[company_id]
+    return dict(posts_top.most_common())
 
 if __name__ == '__main__':
     main()
