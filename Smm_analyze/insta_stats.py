aac29e0ea3f5f9dab76d75d9cbc470c641d25ed1
FaHoLo <47195343+FaHoLo@users.noreply.github.com>
2019-09-28 23:22:38 +0300

    Lower cyclomatic complexity

--- Smm_analyze/insta_stats.py
+++ Smm_analyze/insta_stats.py
@@ -24,7 +24,7 @@ def get_instagram_stats():
     company_name = os.getenv('COMPANY_NAME_INSTAGRAM')
     company_id = int(bot.get_user_id_from_username(company_name))
     all_post_ids = bot.get_total_user_medias(company_id)
-    all_comments = get_all_comments_insta(bot, all_post_ids)
+    all_comments = {post_id: bot.get_media_comments_all(post_id) for post_id in all_post_ids}
     latest_comments = collect_latest_comments_insta(all_comments)
     comments_top = collect_comments_top(latest_comments, company_id)
     posts_top = collect_posts_top(latest_comments, company_id)
@@ -35,39 +35,29 @@ def log_into_instagram(bot):
     insta_password = os.getenv('INSTA_PASSWORD')
     bot.login(username=insta_login, password=insta_password)
 
-def get_all_comments_insta(bot, all_posts):
-    all_comments = {}
-    for post_id in all_posts:
-        all_comments[post_id] = bot.get_media_comments_all(post)
-    return all_comments
-
 def collect_latest_comments_insta(all_comments, days_number=90):
     now_time = datetime.utcnow()
-    latest_comments = {}
-    for post, comments in all_comments.items():
-        post_latest_comments = []
-        for comment in comments:
-            comment_time = datetime.fromtimestamp(comment['created_at_utc'])
-            if now_time - comment_time > timedelta(days=days_number): continue
-            post_latest_comments.append(comment['user_id'])
-        latest_comments.update({post: post_latest_comments})
+    latest_comments = {
+        post_id: [
+            comment['user_id'] for comment in comments
+            if now_time - datetime.fromtimestamp(comment['created_at_utc']) <= timedelta(days=days_number)
+        ]
+        for post_id, comments in all_comments.items()
+    }
     return latest_comments
 
 def collect_comments_top(comments, company_id):
     comments_top = Counter()
     for commenters in comments.values():
-        for commenter in commenters:
-            comments_top[commenter] += 1
+        comments_top.update(commenters)
     del comments_top[company_id]
     return dict(comments_top.most_common())
 
 def collect_posts_top(comments, company_id):
     posts_top = Counter()
     for commenters in comments.values():
-        post_commenters = Counter()
-        for commenter in commenters:
-            post_commenters[commenter] = 1
-        posts_top += post_commenters
+        post_commenters = {commenter for commenter in commenters}
+        posts_top.update(post_commenters)
     del posts_top[company_id]
     return dict(posts_top.most_common())
 
